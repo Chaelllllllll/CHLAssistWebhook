c9db2c3d0c0480a76b11b8517b9ac9eeced09d98
John Michael Manlangit <114377074+Chaelllllllll@users.noreply.github.com>
2024-07-07 14:36:04 +0000

    Upd

--- index.js
+++ index.js
@@ -1,39 +1,90 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
 
-app.use(bodyParser.json());
+    const express = require('express');
+    const bodyParser = require('body-parser');
 
-     app.get('/webhook', (req, res) => {
-       let VERIFY_TOKEN = "chlai";
-       let mode = req.query['hub.mode'];
-       let token = req.query['hub.verify_token'];
-       let challenge = req.query['hub.challenge'];
+    const app = express();
+    const PORT = process.env.PORT || 3000;
+    const VERIFY_TOKEN = 'chlai';
+    const PAGE_ACCESS_TOKEN = 'your_page_access_token_here';
 
-       if (mode && token) {
-         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-           console.log('WEBHOOK_VERIFIED');
-           res.status(200).send(challenge);
-         } else {
-           res.sendStatus(403);
-         }
-       }
-     });
+    app.use(bodyParser.json());
 
-     app.post('/webhook', (req, res) => {
-       let body = req.body;
-       if (body.object === 'page') {
-         body.entry.forEach(function(entry) {
-           let webhook_event = entry.messaging[0];
-           console.log(webhook_event);
-         });
-         res.status(200).send('EVENT_RECEIVED');
-       } else {
-         res.sendStatus(404);
-       }
-     });
+    // Webhook verification
+    app.get('/webhook', (req, res) => {
+        let mode = req.query['hub.mode'];
+        let token = req.query['hub.verify_token'];
+        let challenge = req.query['hub.challenge'];
 
-     app.listen(3000, () => {
-       console.log('Server is running on port 3000');
-     });
-                
+        if (mode && token) {
+            if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+                console.log('WEBHOOK_VERIFIED');
+                res.status(200).send(challenge);
+            } else {
+                res.sendStatus(403);
+            }
+        }
+    });
+
+    // Webhook to handle messages
+    app.post('/webhook', (req, res) => {
+        let body = req.body;
+
+        if (body.object === 'page') {
+            body.entry.forEach(entry => {
+                let webhookEvent = entry.messaging[0];
+                console.log(webhookEvent);
+
+                let senderId = webhookEvent.sender.id;
+                if (webhookEvent.message) {
+                    handleMessage(senderId, webhookEvent.message);
+                }
+            });
+
+            res.status(200).send('EVENT_RECEIVED');
+        } else {
+            res.sendStatus(404);
+        }});
+
+    function handleMessage(senderId, receivedMessage) {
+        let response;
+
+        if (receivedMessage.text) {
+            response = {
+                "text": `You sent the message: "${receivedMessage.text}". Now send me an image!`
+            };
+        } else if (receivedMessage.attachments) {
+            response = {
+                "text": "Thanks for the attachment!"
+            };
+        }
+
+        callSendAPI(senderId, response);
+    }
+
+    function callSendAPI(senderId, response) {
+        const request = require('request');
+
+        let requestBody = {
+            "recipient": {
+                "id": senderId
+            },
+            "message": response
+        };
+
+        request({
+            "uri": "https://graph.facebook.com/v2.6/me/messages",
+            "qs": { "access_token": PAGE_ACCESS_TOKEN },
+            "method": "POST",
+            "json": requestBody
+        }, (err, res, body) => {
+            if (!err) {
+                console.log('Message sent!');
+            } else {
+                console.error('Unable to send message:' + err);
+            }
+        });
+    }
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
